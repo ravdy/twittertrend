c5f00a2ce3ce829ab2984ac9fe00390a660d9391
AR Shankar <valaxytech@gmail.com>
2023-01-21 07:50:20 +0530

    twittertrend v1

---
+++ src/test/java/com/valaxy/demo/DemoWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stalin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
