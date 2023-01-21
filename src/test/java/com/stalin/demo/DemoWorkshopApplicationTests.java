56f18db832bc77e3bda43fb6a471bc21fae9e7a5
AR Shankar <valaxytech@gmail.com>
2023-01-21 10:02:07 +0530

    v2-ttrend code

--- src/test/java/com/stalin/demo/DemoWorkshopApplicationTests.java
+++ src/test/java/com/stalin/demo/DemoWorkshopApplicationTests.java
@@ -3,11 +3,14 @@ package com.stalin.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.*;
+
 @SpringBootTest
 class DemoWorkshopApplicationTests {
 
 	@Test
 	void contextLoads() {
+		List li = new ArrayList<String> ();
 	}
 
 }
