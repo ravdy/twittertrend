c3da1d76f4f098dac8c1fad2e637ced47db7d753
AR Shankar <valaxytech@gmail.com>
2023-02-12 07:15:19 +0530

    paused test and sonar analysis stage

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-kubectl apply -f namespace.yaml
-kubectl create secret generic regcred --from-file=.dockerconfigjson=/root/.docker/config.json --type=kubernetes.io/dockerconfigjson -n valaxy
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
