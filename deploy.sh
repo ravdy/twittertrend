8c456f0a7797e0a16af7fca939c90c828e669f2f
AR Shankar <valaxytech@gmail.com>
2023-02-12 06:35:55 +0530

    updated jenkinsfile with deployment step

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 kubectl apply -f namespace.yaml
-kubectl apply -f secret.yaml
+kubectl create secret generic regcred --from-file=.dockerconfigjson=/root/.docker/config.json --type=kubernetes.io/dockerconfigjson -n valaxy
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
